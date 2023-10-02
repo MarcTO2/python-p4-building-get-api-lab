75fd44ef1e4b7025ec1bd2529c8bfd0691199418
Absalom <mrclabsalom@gmail.com>
2023-10-02 07:16:54 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -2,37 +2,62 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
-
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
+# Initialize database and migrations
+db.init_app(app)
 migrate = Migrate(app, db)
 
-db.init_app(app)
-
+# Routes
 @app.route('/')
 def index():
     return '<h1>Bakery GET API</h1>'
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    all_bakeries = Bakery.query.all()
+    bakery_list = [bakery.to_dict() for bakery in all_bakeries]
+    
+    response = make_response(jsonify(bakery_list), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    specific_bakery = Bakery.query.filter_by(id=id).first()
+    if specific_bakery:
+        bakery_dict = specific_bakery.to_dict()
+        response = make_response(jsonify(bakery_dict), 200)
+    else:
+        response = make_response(jsonify({'message': 'Bakery not found'}), 404)
+    
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = [bg.to_dict() for bg in baked_goods]
+    
+    response = make_response(jsonify(baked_goods_list), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive:
+        most_expensive_dict = most_expensive.to_dict()
+        response = make_response(jsonify(most_expensive_dict), 200)
+    else:
+        response = make_response(jsonify({'message': 'No baked goods found'}), 404)
+    
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
